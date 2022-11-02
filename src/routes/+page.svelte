6ebfde63ab29c2503fdd7f4ee992fd147e0f5d52
Nico-Mayer <nico.m94@icloud.com>
2022-11-02 19:48:31 +0100

    scrolling added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,26 +3,69 @@
   import { slide } from 'svelte/transition'
 
   let isFocused = false
+  let answerBox
   let searchTerm = ''
   let filteredAnswers = []
+  let highlightIndex = 0
 
   $: {
     if (searchTerm) {
       filteredAnswers = $answers.filter((answer) =>
         answer.name.toLowerCase().startsWith(searchTerm.toLowerCase())
       )
+      highlightIndex = 0
     } else {
       filteredAnswers = [...$answers]
+      highlightIndex = 0
     }
   }
 
   function handleEnter(e) {
     // Todo handle all cases when enter is pressed on focus
     if (e.key === 'Enter') {
-      console.log('enter ress')
+      searchTerm = document.querySelector('#btnIndex' + highlightIndex)
+        .childNodes[2].innerHTML
+    }
+  }
+
+  var isItIn = function (parent, child) {
+    var box1coords = parent.getBoundingClientRect()
+    var box2coords = child.getBoundingClientRect()
+
+    if (
+      box2coords.top < box1coords.top ||
+      box2coords.right > box1coords.right ||
+      box2coords.bottom > box1coords.bottom ||
+      box2coords.left < box1coords.left
+    ) {
+      return false
+    }
+    return true
+  }
+
+  function handleArrowKeys(e) {
+    if (isFocused) {
+      switch (e.key) {
+        case 'ArrowUp':
+          if (highlightIndex > 0) {
+            highlightIndex -= 1
+          }
+          break
+        case 'ArrowDown':
+          if (highlightIndex < filteredAnswers.length - 1) {
+            highlightIndex += 1
+          }
+          break
+      }
+      let highlightedBtn = document.querySelector('#btnIndex' + highlightIndex)
+      if (!isItIn(answerBox, highlightedBtn)) {
+        highlightedBtn.scrollIntoView()
+      }
     }
   }
 </script>
+
+<svelte:window on:keydown={handleArrowKeys} />
 
 <main class="flex flex-1 items-center flex-col space-y-22 pt-20">
   <h1 class="text-6xl md:text-7xl font-sans font-bold text-white">Proggle</h1>
@@ -32,16 +75,22 @@
       type="text"
       placeholder="Type programming language..."
       on:focus={() => (isFocused = true)}
-      on:blur={() => (isFocused = false)}
+      on:blur={() => {
+        highlightIndex = 0
+        isFocused = false
+      }}
       on:keypress={handleEnter}
       bind:value={searchTerm} />
     {#if isFocused}
       <div
-        class="absolute w-[90%] md:w-1/2 flex flex-col top-16 max-h-64 overflow-y-scroll bg-[#2D333B] shadow-xl border-t border-white/10 px-5 py-2 transition-all duration-500 rounded-b-xl"
+        class="absolute w-[90%] md:w-1/2 flex flex-col top-16 max-h-80 overflow-y-scroll bg-[#2D333B] shadow-xl border-t border-white/10 p-2 transition-all duration-500 rounded-b-xl scrollbar-hide space-y-2"
+        bind:this={answerBox}
         transition:slide>
-        {#each filteredAnswers as answer}
+        {#each filteredAnswers as answer, index}
           <button
-            class="flex text-white/60 items-center space-x-4 hover:(bg-white/10 text-white) rounded-xl p-5"
+            class="flex text-white/60 items-center space-x-4 hover:(bg-white/10 text-white) rounded-xl p-5 h-14"
+            class:highlighted={index === highlightIndex}
+            id={'btnIndex' + index}
             on:mousedown={() => (searchTerm = answer.name)}>
             <img src={answer.iconUrl} class="w-5" alt="icon" />
             <span>{answer.name}</span>
@@ -51,3 +100,9 @@
     {/if}
   </div>
 </main>
+
+<style>
+  .highlighted {
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+</style>
