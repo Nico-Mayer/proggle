3fa1f06c595d793e3c70fb9f3bb34d91529ec5a7
Nico-Mayer <nico.m94@icloud.com>
2022-11-28 20:13:43 +0100

    improvements to the searchbar logic

---
+++ src/lib/SearchBox.svelte
@@ -0,0 +1,160 @@
+<script>
+  import { slide } from 'svelte/transition'
+  import isItIn from '$lib/utils/isItIn.js'
+  import { createEventDispatcher } from 'svelte'
+  export let answers
+
+  const dispatch = createEventDispatcher()
+
+  let playShake = false
+  let isFocused = false
+  let answerBox
+  let inputField
+  let searchTerm = ''
+  let highlightIndex = 0
+  let filteredAnswers = []
+
+  $: {
+    if (searchTerm) {
+      filteredAnswers = answers
+        .filter((answer) =>
+          answer.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+        )
+        .sort((a, b) => a.name.localeCompare(b.name))
+      highlightIndex = 0
+    } else {
+      filteredAnswers = [...answers]
+      highlightIndex = 0
+    }
+  }
+
+  function handleEnter(e) {
+    // Todo handle all cases when enter is pressed on focus
+    if (e.key === 'Enter' && filteredAnswers.length > 0 && isFocused) {
+      let highlightBtnText = document.querySelector(
+        '#btnIndex' + highlightIndex
+      ).childNodes[2].innerHTML
+      searchTerm = highlightBtnText
+
+      if (searchTerm.toLowerCase() === filteredAnswers[0].name.toLowerCase()) {
+        // Anwort abschicken
+        // Add Guess to list
+        dispatch('submit', filteredAnswers[0])
+        inputField.blur()
+        searchTerm = ''
+
+        console.log(
+          'Abschicken ID:' +
+            filteredAnswers[0]._id +
+            ' Name: ' +
+            filteredAnswers[0].name
+        )
+      }
+    } else if (e.key === 'Enter' && filteredAnswers.length <= 0 && isFocused) {
+      playShake = true
+      setTimeout(() => (playShake = false), 820)
+    }
+  }
+
+  function handleArrowKeys(e) {
+    if (isFocused && filteredAnswers.length > 0) {
+      switch (e.key) {
+        case 'ArrowUp':
+          if (highlightIndex > 0) {
+            highlightIndex -= 1
+          }
+          break
+        case 'ArrowDown':
+          if (highlightIndex < filteredAnswers.length - 1) {
+            highlightIndex += 1
+          }
+          break
+      }
+      let highlightedBtn = document.querySelector('#btnIndex' + highlightIndex)
+      if (!isItIn(answerBox, highlightedBtn)) {
+        highlightedBtn.scrollIntoView()
+      }
+    }
+  }
+</script>
+
+<svelte:window on:keydown={handleArrowKeys} />
+
+<div class="flex flex-col font-mono w-[90%] items-center md:w-1/2">
+  <section class="flex space-x-4 w-full">
+    <input
+      class="rounded-xl outline-none bg-[#2D333B] h-16 shadow-xl w-full p-5 placeholder:text-white/20 caret-white/60"
+      type="text"
+      placeholder="Type programming language..."
+      class:apply-shake={playShake}
+      on:focus={() => (isFocused = true)}
+      on:blur={() => {
+        highlightIndex = 0
+        isFocused = false
+      }}
+      on:keypress={handleEnter}
+      bind:this={inputField}
+      bind:value={searchTerm} />
+    <button class="rounded-xl bg-green-600 shadow-xl py-5 px-12">Try</button>
+  </section>
+
+  {#if isFocused && filteredAnswers.length > 0}
+    <div
+      class="rounded-xl flex flex-col bg-[#2D333B] shadow-xl mt-4 w-full max-h-80 overflow-y-scroll scrollbar-hide"
+      bind:this={answerBox}
+      transition:slide>
+      {#each filteredAnswers as answer, index}
+        <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+        <button
+          class="flex space-x-6 border-6 border-[#2D333B] rounded-2xl h-18 p-5 text-white/60 items-center group hover:text-white"
+          class:highlightedBtn={index === highlightIndex}
+          id={'btnIndex' + index}
+          on:mousedown={() => (searchTerm = answer.name)}>
+          <img
+            src={answer.iconUrl}
+            class="opacity-60 w-5 group-hover:opacity-100"
+            class:hundertop={index === highlightIndex}
+            alt="icon" />
+          <span>{answer.name}</span>
+        </button>
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style>
+  @keyframes shake {
+    10%,
+    90% {
+      transform: translate3d(-1px, 0, 0);
+    }
+
+    20%,
+    80% {
+      transform: translate3d(2px, 0, 0);
+    }
+
+    30%,
+    50%,
+    70% {
+      transform: translate3d(-4px, 0, 0);
+    }
+
+    40%,
+    60% {
+      transform: translate3d(4px, 0, 0);
+    }
+  }
+  .hundertop {
+    opacity: 100;
+  }
+
+  .apply-shake {
+    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  }
+
+  .highlightedBtn {
+    background-color: rgba(0, 0, 0, 0.05);
+    color: white;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,81 +1,32 @@
 <script>
-  import { slide } from 'svelte/transition'
-  import isItIn from '$lib/utils/isItIn.js'
+  import Searchbox from '$lib/SearchBox.svelte'
+  import { onMount } from 'svelte'
   export let data
-
   const { answers } = data
 
-  let playShake = false
-  let isFocused = false
-  let answerBox
-  let inputField
-  let searchTerm = ''
-  let filteredAnswers = []
-  let highlightIndex = 0
+  let validAnswers = []
+  let guessed = []
+
+  onMount(() => {
+    validAnswers = [...answers]
+    let localGuesses = JSON.parse(localStorage.getItem('GUESSED'))
+    guessed = localGuesses ? localGuesses : []
+
+    for (let i = 0; i < guessed.length; i++) {
+      let index = validAnswers.findIndex((el) => el.name == guessed[i].name)
+      validAnswers.splice(index, 1)
+    }
+  })
 
   $: {
-    if (searchTerm) {
-      filteredAnswers = answers
-        .filter((answer) =>
-          answer.name.toLowerCase().startsWith(searchTerm.toLowerCase())
-        )
-        .sort((a, b) => a.name.localeCompare(b.name))
-
-      highlightIndex = 0
-    } else {
-      filteredAnswers = [...answers]
-      highlightIndex = 0
-    }
+    validAnswers = validAnswers.filter((n) => !guessed.includes(n))
   }
 
-  function handleEnter(e) {
-    // Todo handle all cases when enter is pressed on focus
-    if (e.key === 'Enter' && filteredAnswers.length > 0 && isFocused) {
-      let highlightBtnText = document.querySelector(
-        '#btnIndex' + highlightIndex
-      ).childNodes[2].innerHTML
-      searchTerm = highlightBtnText
-
-      if (searchTerm.toLowerCase() === filteredAnswers[0].name.toLowerCase()) {
-        // Anwort abschicken
-        inputField.blur()
-        searchTerm = ''
-        console.log(
-          'Abschicken ID:' +
-            filteredAnswers[0]._id +
-            ' Name: ' +
-            filteredAnswers[0].name
-        )
-      }
-    } else if (e.key === 'Enter' && filteredAnswers.length <= 0 && isFocused) {
-      playShake = true
-      setTimeout(() => (playShake = false), 820)
-    }
-  }
-
-  function handleArrowKeys(e) {
-    if (isFocused && filteredAnswers.length > 0) {
-      switch (e.key) {
-        case 'ArrowUp':
-          if (highlightIndex > 0) {
-            highlightIndex -= 1
-          }
-          break
-        case 'ArrowDown':
-          if (highlightIndex < filteredAnswers.length - 1) {
-            highlightIndex += 1
-          }
-          break
-      }
-      let highlightedBtn = document.querySelector('#btnIndex' + highlightIndex)
-      if (!isItIn(answerBox, highlightedBtn)) {
-        highlightedBtn.scrollIntoView()
-      }
-    }
+  function handleSubmit(event) {
+    guessed = [event.detail, ...guessed]
+    localStorage.setItem('GUESSED', JSON.stringify(guessed))
   }
 </script>
-
-<svelte:window on:keydown={handleArrowKeys} />
 
 <main class="flex flex-col space-y-14 flex-1 pt-20 items-center">
   <div class="flex flex-col items-center">
@@ -84,83 +35,10 @@
     </h1>
     <span class="font-mono text-lg text-white/60">New language every day!</span>
   </div>
-
-  <div class="flex flex-col font-mono w-[90%] items-center md:w-1/2">
-    <section class="flex space-x-4 w-full">
-      <input
-        class="rounded-xl outline-none bg-[#2D333B] h-16 shadow-xl w-full p-5 placeholder:text-white/20 caret-white/60"
-        type="text"
-        placeholder="Type programming language..."
-        class:apply-shake={playShake}
-        on:focus={() => (isFocused = true)}
-        on:blur={() => {
-          highlightIndex = 0
-          isFocused = false
-        }}
-        on:keypress={handleEnter}
-        bind:this={inputField}
-        bind:value={searchTerm} />
-      <button class="rounded-xl bg-green-600 shadow-xl py-5 px-12">Try</button>
-    </section>
-
-    {#if isFocused && filteredAnswers.length > 0}
-      <div
-        class="rounded-xl flex flex-col bg-[#2D333B] shadow-xl mt-4 w-full max-h-80 overflow-y-scroll scrollbar-hide"
-        bind:this={answerBox}
-        transition:slide>
-        {#each filteredAnswers as answer, index}
-          <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-          <button
-            class="flex space-x-6 border-6 border-[#2D333B] rounded-2xl h-18 p-5 text-white/60 items-center group hover:text-white"
-            class:highlightedBtn={index === highlightIndex}
-            id={'btnIndex' + index}
-            on:mousedown={() => (searchTerm = answer.name)}>
-            <img
-              src={answer.iconUrl}
-              class="opacity-60 w-5 group-hover:opacity-100"
-              class:hundertop={index === highlightIndex}
-              alt="icon" />
-            <span>{answer.name}</span>
-          </button>
-        {/each}
-      </div>
-    {/if}
+  <Searchbox answers={validAnswers} on:submit={handleSubmit} />
+  <div>
+    {#each guessed as guess}
+      {guess.name}
+    {/each}
   </div>
 </main>
-
-<style>
-  @keyframes shake {
-    10%,
-    90% {
-      transform: translate3d(-1px, 0, 0);
-    }
-
-    20%,
-    80% {
-      transform: translate3d(2px, 0, 0);
-    }
-
-    30%,
-    50%,
-    70% {
-      transform: translate3d(-4px, 0, 0);
-    }
-
-    40%,
-    60% {
-      transform: translate3d(4px, 0, 0);
-    }
-  }
-  .hundertop {
-    opacity: 100;
-  }
-
-  .apply-shake {
-    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
-  }
-
-  .highlightedBtn {
-    background-color: rgba(0, 0, 0, 0.05);
-    color: white;
-  }
-</style>
